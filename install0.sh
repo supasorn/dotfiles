9080a7cc894024f1710b8d4912acea120c2cdfb5
Supasorn Suwajanakorn <supasorn@gmail.com>
2022-12-16 18:49:57 +0700

    auto commit

--- install0.sh
+++ install0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/dotfiles/master/install0.sh)"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get update
 sudo apt -y install neovim vim zsh tmux curl ripgrep git exuberant-ctags nodejs unzip
