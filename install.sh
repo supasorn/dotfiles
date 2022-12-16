50de0336d0b69971e0c0db6e032222175b222ff1
Supasorn Suwajanakorn <supasorn@gmail.com>
2022-12-16 17:42:18 +0700

    auto commit

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 ln -sf /dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 
-mkdir -p ~/.config/nvim
-ln -sf ~/.vim/init.vim ~/.config/nvim/init.vim
-
 mkdir -p ~/.config/lf
 ln -sf ~/dotfiles/lfrc ~/.config/lf/lfrc
 ln -sf ~/dotfiles/lfcd.sh ~/.config/lf/lfcd.sh
