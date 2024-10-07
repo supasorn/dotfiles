3284b09ce439802832acb72335406adf4262d3ed
supasorn <supasorn@gmail.com>
2024-10-07 13:24:17 +0700

    auto commit

---
+++ install_singularity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home2/supasorn
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    libssl-dev \
+    uuid-dev \
+    libgpgme11-dev \
+    squashfs-tools \
+    libseccomp-dev \
+    pkg-config \
+    libfuse3-dev \
+    libglib2.0-dev
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+
