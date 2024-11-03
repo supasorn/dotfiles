13f347dfcf1c546dfcdb20f0907aaf62ca8eaafe
Supasorn Suwajanakorn <supasorn@gmail.com>
2024-11-03 16:18:42 +0700

    auto commit

--- create_supasorn2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo useradd -M -s /bin/bash supasorn2
-sudo passwd supasorn2
-sudo usermod -aG sudo supasorn2
-
--- reinstall_nvim.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ~/.config/nvim ~/.local/share/nvim ~/.cache/nvim ~/dotfiles/nvim
-rm -rf ~/dotfiles/nvim
-
-git clone https://github.com/supasorn/nvim ~/dotfiles/nvim
-ln -sf ~/dotfiles/nvim -T ~/.config/nvim
-nvim 
-
