14b17a3463b1e258109046d6040d51a6040da4c3
supasorn <supasorn@gmail.com>
2024-11-27 21:51:08 +0700

    auto commit

--- scripts/uidgid.sh
+++ scripts/uidgid.sh
@@ -6,17 +6,18 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Check for required arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <username> <new_id>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <username> <new_id> <new_gid>"
     exit 1
 fi
 
 USERNAME=$1
 NEW_ID=$2
+NEW_GID=$3
 
-# Check if the new ID is already in use
-if id -u "$NEW_ID" &>/dev/null || getent group "$NEW_ID" &>/dev/null; then
-    echo "Error: The ID $NEW_ID is already in use as a UID or GID. Please choose a different ID."
+# Check if the new ID or GID is already in use
+if id -u "$NEW_ID" &>/dev/null || getent group "$NEW_GID" &>/dev/null; then
+    echo "Error: The ID $NEW_ID or GID $NEW_GID is already in use. Please choose different values."
     exit 1
 fi
 
@@ -48,10 +49,11 @@ fi
 # Display the current UID and GID
 echo "Current UID: $OLD_UID"
 echo "Current GID: $OLD_GID"
-echo "New UID and GID: $NEW_ID"
+echo "New UID: $NEW_ID"
+echo "New GID: $NEW_GID"
 
-groupmod -g "$NEW_ID" "$USERNAME"
-usermod -u "$NEW_ID" -g "$NEW_ID" "$USERNAME"
+groupmod -g "$NEW_GID" "$USERNAME"
+usermod -u "$NEW_ID" -g "$NEW_GID" "$USERNAME"
 
 # Update file ownership in the specified directories
 DIRECTORIES=("/home/$USERNAME" "/home2/$USERNAME" "/data/$USERNAME" "/data2/$USERNAME")
@@ -63,8 +65,9 @@ for DIR in "${DIRECTORIES[@]}"; do
         find "$DIR" -xdev -user "$OLD_UID" -exec chown -h "$NEW_ID" {} +
 
         echo "Updating files with old GID $OLD_GID in $DIR..."
-        find "$DIR" -xdev -group "$OLD_GID" -exec chgrp -h "$NEW_ID" {} +
+        find "$DIR" -xdev -group "$OLD_GID" -exec chgrp -h "$NEW_GID" {} +
     else
         echo "Directory $DIR does not exist, skipping..."
     fi
 done
+
