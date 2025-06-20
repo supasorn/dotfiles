b6d3b3fe4bbf5bc32c3f0a2984004d2f359a30e2
Supasorn Suwajanakorn <supasorn@gmail.com>
2025-06-20 23:48:33 +0700

    auto commit

--- scripts/tmux_img3d.sh
+++ scripts/tmux_img3d.sh
@@ -9,5 +9,5 @@ split-window -v \; \
 select-pane -t 1 \; send-keys "\ssh -t v3 'zsh -i -c \"r --sing img3dviewer\"'" C-m \; \
 select-pane -t 2 \; send-keys "\ssh -t v3 'zsh -i -c \"r --sing dwui 1\"'" C-m \; \
 select-pane -t 3 \; send-keys "\ssh -t v6 'zsh -i -c \"r --sing depth_anything 1\"'" C-m \; \
-select-pane -t 4 \; send-keys "\ssh -t v4 'zsh -i -c \"r --sing video_depth_anything 1\"'" C-m  \; \
+select-pane -t 4 \; send-keys "\ssh -t v2 'zsh -i -c \"r --sing video_depth_anything 0\"'" C-m  \; \
 select-pane -t 5 \; send-keys "\ssh -t v3 'zsh -i -c \"cd /data/supasorn/img3dviewer && exec zsh\"'" C-m 
