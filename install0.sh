4a9b015867ab2558a63265d406dd9446448842bf
supasorn <supasorn@gmail.com>
2025-08-04 12:03:49 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -2,7 +2,7 @@
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/dotfiles/master/install0.sh)"
 cd ~
 git clone https://github.com/supasorn/dotfiles
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 sudo apt-get update
 sudo apt -y install neovim vim zsh tmux curl ripgrep git exuberant-ctags nodejs unzip
 chsh -s /usr/bin/zsh
