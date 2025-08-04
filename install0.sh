c7ea62ed33d66f5627823a68d7fa043c42a733ef
supasorn <supasorn@gmail.com>
2025-08-04 12:05:21 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -4,7 +4,7 @@ cd ~
 git clone https://github.com/supasorn/dotfiles
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 sudo apt-get update
-sudo apt -y install neovim vim zsh tmux curl ripgrep git exuberant-ctags nodejs unzip
+sudo apt -y install zsh tmux ripgrep git exuberant-ctags unzip
 chsh -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/dotfiles/install.sh
